55c5172fccee9579fedd40f7bcf662bb04656439
Nick Jackson <nick@nickjackson.me>
2017-10-18 13:12:13 +0100

    Initial scraper

--- scraper.py
+++ scraper.py
@@ -1,24 +1,55 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# coding=utf-8
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import lxml.html
+import sqlite3
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+BASE_URL = 'http://www.diputadossantafe.gov.ar/web/camara/diputados'
+
+PAGES = 5
+
+page = 1
+
+parsedMembers = []
+
+while page <= PAGES:
+
+    print 'Page ' + str(page)
+
+    html = scraperwiki.scrape(BASE_URL + '?page=' + str(page))
+
+    root = lxml.html.fromstring(html)
+    members = root.cssselect('div[class=\'autoridad-little\']')
+
+    for member in members:
+
+        memberData = {}
+
+        memberData['image'] = 'http://www.diputadossantafe.gov.ar' + member.cssselect('img')[0].attrib['src']
+
+        name = member.cssselect('h4')[0].text
+
+        #  This seems to be very consistently Last, First
+        nameParts = name.split(', ')
+
+        memberData['name'] = u'{} {}'.format(nameParts[1], nameParts[0])
+        memberData['first_name'] = nameParts[1]
+        memberData['last_name'] = nameParts[0]
+
+        memberData['party'] = member.cssselect('h5')[0].text.replace('BLOQUE ', '')
+
+        print memberData
+
+        parsedMembers.append(memberData)
+
+    page += 1
+
+print 'Counted {} Members'.format(len(parsedMembers))
+
+try:
+    scraperwiki.sqlite.execute('DELETE FROM data')
+except sqlite3.OperationalError:
+    pass
+scraperwiki.sqlite.save(
+    unique_keys=['name'],
+    data=parsedMembers)
